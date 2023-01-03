9973bc69034191f64a14bd14eae254a0f25805a6
RthR2 <aarthurpatriarca@gmail.com>
2023-01-03 11:55:16 -0300

    add try/catch

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -6,41 +7,47 @@ public class Main {
 
         System.out.println("++++++++++++ Calculadora ++++++++++++++++++++");
 
-        System.out.println("\n[1] Soma" +
-                "\n[2] Subtração" +
-                "\n[3] Multiplicação" +
-                "\n[4] Divisão\n"
+        System.out.println("""
+                [1] Soma
+                [2] Subtração
+                [3] Multiplicação
+                [4] Divisão
+                """
         );
         System.out.print("Qual operação deseja fazer: [1][2][3][4] ");
-        int operation = scanner.nextInt();
 
-        if(operation <= 0 || operation > 4){
-            System.out.println("Operação inválido! Digite o número de uma operação válida: [1][2][3][4]");
-        } else {
-            System.out.print("Digite o primeiro valor: ");
-            double num1 = scanner.nextDouble();
-            System.out.print("Digite o segundo valor: ");
-            double num2 = scanner.nextDouble();
+        try {
+            int operation = scanner.nextInt();
 
-            double sum = num1 + num2;
-            double sub = num1 - num2;
-            double multi = num1 * num2;
-            double div  = num1 / num2;
+            if (operation <= 0 || operation > 4) {
+                System.out.println("Operação inválido! Digite o número de uma operação válida: [1][2][3][4]");
+            } else {
+                System.out.print("Digite o primeiro valor: ");
+                double num1 = scanner.nextDouble();
+                System.out.print("Digite o segundo valor: ");
+                double num2 = scanner.nextDouble();
 
-            System.out.println("");
+                double sum = num1 + num2;
+                double sub = num1 - num2;
+                double multi = num1 * num2;
+                double div = num1 / num2;
 
-            if (operation == 1) {
-                System.out.println(num1 + " + " + num2 + " = " + " " + sum);
-            } else if (operation == 2) {
-                System.out.println(num1 + " - " + num2 + " = " + " " + sub);
-            } else if (operation == 3) {
-                System.out.println(num1 + " * " + num2 + " = " + " " + multi);
-            } else if (operation == 4 && num2 != 0) {
-                System.out.println(num1 + " / " + num2 + " = " + " " + div);
-            } else if (operation == 4 && num2 == 0){
-                System.out.println("Erro, tente novamente! Divisão por zero");
+                System.out.println(" ");
+
+                if (operation == 1) {
+                    System.out.println(num1 + " + " + num2 + " = " + sum);
+                } else if (operation == 2) {
+                    System.out.println(num1 + " - " + num2 + " = " + sub);
+                } else if (operation == 3) {
+                    System.out.println(num1 + " * " + num2 + " = " + multi);
+                } else if (operation == 4 && num2 != 0) {
+                    System.out.println(num1 + " / " + num2 + " = " + div);
+                } else if (operation == 4 && num2 == 0) {
+                    System.out.println("Erro, tente novamente! Divisão por zero");
+                }
             }
+        } catch (InputMismatchException e) {
+            System.out.println("Ops... Você digitou caracteres. Tente novamente, e digite uma operação válida: [1][2][3][4]");
         }
-
     }
 }
