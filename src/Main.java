0e8177333b73a0e4a196c722a42763ae77a38474
RthR2 <aarthurpatriarca@gmail.com>
2023-01-03 14:44:39 -0300

    add loop

--- src/Main.java
+++ src/Main.java
@@ -7,47 +7,55 @@ public class Main {
 
         System.out.println("++++++++++++ Calculadora ++++++++++++++++++++");
 
-        System.out.println("""
-                [1] Soma
-                [2] Subtração
-                [3] Multiplicação
-                [4] Divisão
-                """
-        );
-        System.out.print("Qual operação deseja fazer: [1][2][3][4] ");
-
         try {
-            int operation = scanner.nextInt();
 
-            if (operation <= 0 || operation > 4) {
-                System.out.println("Operação inválido! Digite o número de uma operação válida: [1][2][3][4]");
-            } else {
-                System.out.print("Digite o primeiro valor: ");
-                double num1 = scanner.nextDouble();
-                System.out.print("Digite o segundo valor: ");
-                double num2 = scanner.nextDouble();
+            int exit = 1;
+            while(exit != 0){
+                //executar o programa
+                System.out.println("""
+                        \n[1] Soma
+                        [2] Subtração
+                        [3] Multiplicação
+                        [4] Divisão
+                        """
+                );
+                System.out.print("Digite a operação desejada: [1][2][3][4] ");
+                int operation = scanner.nextInt();
 
-                double sum = num1 + num2;
-                double sub = num1 - num2;
-                double multi = num1 * num2;
-                double div = num1 / num2;
+                if (operation <= 0 || operation > 4) {
+                    System.out.println("Operação inválido! Digite o número de uma operação válida: [1][2][3][4] \n");
+                } else {
+                    System.out.print("Digite o primeiro valor: ");
+                    double num1 = scanner.nextDouble();
+                    System.out.print("Digite o segundo valor: ");
+                    double num2 = scanner.nextDouble();
 
-                System.out.println(" ");
+                    double sum = num1 + num2;
+                    double sub = num1 - num2;
+                    double multi = num1 * num2;
+                    double div = num1 / num2;
 
-                if (operation == 1) {
-                    System.out.println(num1 + " + " + num2 + " = " + sum);
-                } else if (operation == 2) {
-                    System.out.println(num1 + " - " + num2 + " = " + sub);
-                } else if (operation == 3) {
-                    System.out.println(num1 + " * " + num2 + " = " + multi);
-                } else if (operation == 4 && num2 != 0) {
-                    System.out.println(num1 + " / " + num2 + " = " + div);
-                } else if (operation == 4 && num2 == 0) {
-                    System.out.println("Erro, tente novamente! Divisão por zero");
+                    System.out.println(" ");
+
+                    if (operation == 1) {
+                        System.out.println(num1 + " + " + num2 + " = " + sum);
+                    } else if (operation == 2) {
+                        System.out.println(num1 + " - " + num2 + " = " + sub);
+                    } else if (operation == 3) {
+                        System.out.println(num1 + " * " + num2 + " = " + multi);
+                    } else if (operation == 4 && num2 != 0) {
+                        System.out.println(num1 + " / " + num2 + " = " + div);
+                    } else if (operation == 4 && num2 == 0) {
+                        System.out.println("Erro, tente novamente! Divisão por zero");
+                    }
                 }
+
+                System.out.println("\nDigite 0(zero) para terminar o programa Ou Digite qualquer numero para fazer outra operação");
+                exit = scanner.nextInt();
             }
         } catch (InputMismatchException e) {
-            System.out.println("Ops... Você digitou caracteres. Tente novamente, e digite uma operação válida: [1][2][3][4]");
+            System.out.println("\nOps... Você digitou algum caracter inválido.\nReinicie a calculadora e tente novamente");
         }
+
     }
 }
